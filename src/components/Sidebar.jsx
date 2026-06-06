55b6cd4a99a32b36ed4da018f03e36befa82218a
Tushal Joshi <joshitushal1@gmail.com>
2026-06-06 15:34:20 +0530

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,7 +31,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
         { label: "Add Banner", icon: ImageIcon, path: "/add-banner" },
         { label: "Add Review", icon: ScanEyeIcon, path: "/add-review" },
         { label: "Create Services", icon: CarFrontIcon, path: "/service" },
-        { label: "Job Required Parts", icon: Wrench, path: "/job-part" },
+        { label: "Add Spare Parts", icon: Wrench, path: "/job-part" },
         { label: "SOS Vehicle Issues", icon: ScanEyeIcon, path: "/visible-issues" },
         { label: "Create Plan", icon: CoinsIcon, path: "/plan" },
         { label: "Mechanic Services", icon: PackageOpenIcon, path: "/mechanicaservices" },
